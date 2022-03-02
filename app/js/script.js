9cdfb85d2fabb340dd2029065c5fe58d059dd5f8
K UDAY KUMAR <72476990+UDAY368@users.noreply.github.com>
2022-03-02 10:32:53 +0530

    Fade In and Fade out effects Completed

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,18 @@
-console.log("I love Web Development");
+const btnHamburger = document.querySelector('#buttonHamburger');
+const header = document.querySelector('.app__header');
+const overlay = document.querySelector('.overlay');
+
+btnHamburger.addEventListener('click', function() {
+  console.log('click Hamburger');
+  
+  if(header.classList.contains('open')){      //close Hamburger menu
+    header.classList.remove('open');
+    overlay.classList.add('fade-out');
+    overlay.classList.remove('fade-in');
+  }
+  else{
+    header.classList.add('open');
+    overlay.classList.add('fade-in');
+    overlay.classList.remove('fade-out');
+  }
+})
